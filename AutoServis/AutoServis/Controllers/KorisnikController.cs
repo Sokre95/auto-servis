ac18d144d1bf7a14d2d0f7e7cd508460c48faa95
mpisacic <martin.pisacic@fer.hr>
2016-12-16 17:24:43 +0100

    Kontroleri i uloge

---
+++ AutoServis/AutoServis/Controllers/KorisnikController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoServis.Controllers
+{
+	[Authorize(Roles = "Korisnik")]
+    public class KorisnikController : Controller
+    {
+        // GET: Korisnik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AutoServis/AutoServis/Controllers/ServiserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoServis.Controllers
+{
+	[Authorize(Roles = "Serviser")]
+    public class ServiserController : Controller
+    {
+        // GET: Serviser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
